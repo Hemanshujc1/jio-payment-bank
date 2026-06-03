b360b7a3423fa7be0a38eec4d94a1a6fcfddd5eb
Hemanshu <hemanshujc01@gmail.com>
2026-06-03 14:35:11 +0530

    fix: update geolocation storage logic to use dynamic coordinates instead of hardcoded values

--- src/pages/SavingsVariantPage.jsx
+++ src/pages/SavingsVariantPage.jsx
@@ -25,11 +25,11 @@ const SavingsVariantPage = () => {
 
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        localStorage.setItem("latitude",28.4210);
-        // localStorage.setItem("latitude", position.coords.latitude.toString().substring(0, 6));
+        // localStorage.setItem("latitude",28.4210);
+        localStorage.setItem("latitude", position.coords.latitude.toString().substring(0, 6));
 
-        localStorage.setItem("longitude", 77.1100);
-        // localStorage.setItem("longitude", position.coords.longitude.toString().substring(0, 6));
+        // localStorage.setItem("longitude", 77.1100);
+        localStorage.setItem("longitude", position.coords.longitude.toString().substring(0, 6));
 
         // Forcefully set VKID so it overwrites the old cached value
         localStorage.setItem("vkid", import.meta.env.VITE_USER_ID);
